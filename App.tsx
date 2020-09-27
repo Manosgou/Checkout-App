a4e3b0c2186df01c0adb1edfbc1611aca057182c
Manosgou <manolisgouvrikos@gmail.com>
2020-09-28 01:43:37 +0300

    Add FloatActionButton component

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//components
+import FloatActionButton from './components/FloatActionButton';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <FloatActionButton/>
     </View>
   );
 }
---
+++ components/FloatActionButton.tsx
@@ -0,0 +1,34 @@
+import React,{ Component } from "react";
+import {StyleSheet,View,TouchableOpacity} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+
+export default class FloatActionButton extends Component{
+    render(){
+        return(
+            <View style={styles.fabContainer}>
+            <TouchableOpacity style={styles.fab}>
+              <AntDesign name="plus" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        );
+    }
+}
+
+const styles =StyleSheet.create({
+    fabContainer: {
+        position: 'absolute',
+        bottom: 5,
+        right: 5
+      },
+    
+      fab: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 70,
+        height: 70,
+        backgroundColor: '#D95525',
+        borderRadius: 50,
+        elevation:5
+      },
+  })
