523b9faf9186581a5ef74cb7de464743e692bbad
Manosgou <manolisgouvrikos@gmail.com>
2020-09-28 01:53:24 +0300

    Add Header component

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView,  Text, View } from 'react-native';
 
 //components
+import Header from './components/Header';
 import FloatActionButton from './components/FloatActionButton';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     <FloatActionButton/>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <Header/>
+      <FloatActionButton/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+
+
+
+
+export default class Header extends Component{
+    render() {
+        return (
+            <View style={styles.header}>
+                <Text style={styles.title}>Check<Text style={{ color: 'black' }}>out</Text></Text>
+                <TouchableOpacity style={styles.trashIcon}>
+                    <FontAwesome5 name="trash-alt" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 75,
+        paddingTop: 10,
+        backgroundColor: '#D95525',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 45,
+        color: 'white',
+        letterSpacing: 4,
+        marginTop: 8
+
+    },
+    trashIcon: {
+        position: 'absolute',
+        right: 15,
+        top: 40
+
+
+    }
+});
